c9f56dfead4289c0e9b22d8cede189483c1633ad
Anh Trinh <vasmogna191004@gmail.com>
2025-01-04 01:50:48 -0800

    added functionality for List section with 12 operations and their own template for sub-items

--- src/Component/List.tsx
+++ src/Component/List.tsx
@@ -1,22 +1,84 @@
+import React, { useState } from "react";
 import "../styling/List.css";
 
 function List() {
+  const [expandItems, setExpandItems] = useState<{ [key: number]: boolean }>({});
+
+  const items = [
+    {
+      name: "Number Systems",
+      subItems: ["Sub-item 1", "Sub-item 2"]
+    },
+    {
+      name: "Boolean Algebra",
+      subItems: ["Sub-item 1", "Sub-item 2"]
+    },
+    {
+      name: "Data Representation",
+      subItems: ["Sub-item 1", "Sub-item 2"]
+    },
+    {
+      name: "Bitwise Operations",
+      subItems: ["Sub-item 1", "Sub-item 2"]
+    },
+    {
+      name: "Algorithmic Calculations",
+      subItems: ["Sub-item 1", "Sub-item 2"]
+    },
+    {
+      name: "Networking",
+      subItems: ["Sub-item 1", "Sub-item 2"]
+    },
+    {
+      name: "Memory Calculations",
+      subItems: ["Sub-item 1", "Sub-item 2"]
+    },
+    {
+      name: "Cryptography",
+      subItems: ["Sub-item 1", "Sub-item 2"]
+    },
+    {
+      name: "Time Complexity",
+      subItems: ["Sub-item 1", "Sub-item 2"]
+    },
+    {
+      name: "Operating Systems",
+      subItems: ["Sub-item 1", "Sub-item 2"]
+    },
+    {
+      name: "Miscellaneous Conversions",
+      subItems: ["Sub-item 1", "Sub-item 2"]
+    }
+  ];
+
+  const toggleItem = (index: number) => {
+    setExpandItems(prev => ({
+        ...prev,
+        [index]: !prev[index]
+    }));
+};
+  
   return (
     <div className="list">
       <div className="list-container">
-        <h1>My List</h1>
-        <ul>
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
-          <li>Item 4</li>
-          <li>Item 5</li>
-          <li>Item 6</li>
-          <li>Item 7</li>
-          <li>Item 8</li>
-          <li>Item 9</li>
-          <li>Item 10</li>
-        </ul>
+      <div className="items-list">
+          {items.map((item, index) => (
+            <div key={index} className="items">
+              <button onClick={() => toggleItem(index)} className="item">
+                <span>{item.name}</span>
+              </button>
+              {expandItems[index] && (
+                <div className="items-expand">
+                  {item.subItems.map((subItem, subIndex) => (
+                    <button key={subIndex} className="sub-item">
+                      {subItem}
+                    </button>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
